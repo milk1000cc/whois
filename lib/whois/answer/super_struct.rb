7c61494e3fa8e15bc919649dc3c01169ddb3b3e3
Simone Carletti <weppos@weppos.net>
2009-12-01 12:40:34 +0100

    yardoc

--- lib/whois/answer/super_struct.rb
+++ lib/whois/answer/super_struct.rb
@@ -31,11 +31,22 @@ class SuperStruct < Struct
   # Overwrites the standard Struct initializer
   # to add the ability to create an instance from a Hash of parameters.
   #
+  # If block is given, yields self.
+  #
   #   attributes = { :foo => 1, :bar => "baz" }
   #   Struct.new(attributes)
   #   # => #<Struct foo=1, bar="baz">
   #
-  # If block is given, the block is called on self.
+  # @overload initialize(value, value, ...)
+  #   Initializes a new instance setting each Struct key to the value
+  #   passed in the same order in the argument list.
+  #   @param [Object] value describe value param
+  # @overload initialize(attributes)
+  #   Initializes a new instance from the key-value Hash.
+  #   @param [Hash<Symbol => Object>] attributes
+  # @return [SuperStruct]
+  # @yield  [self] a block to perform any extra initialization on the object
+  # @yieldparam [Client] self the newly initialized client object
   #
   def initialize(*args, &block)
     if args.first.is_a? Hash
